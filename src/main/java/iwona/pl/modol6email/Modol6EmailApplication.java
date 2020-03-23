d6758ea05e03f86d3e46b6ba5be4e01e97844425
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-03-23 09:55:54 +0100

    Initial comit

---
+++ src/main/java/iwona/pl/modol6email/Modol6EmailApplication.java
@@ -0,0 +1,13 @@
+package iwona.pl.modol6email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modol6EmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Modol6EmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iwona/pl/modol6email/Modol6EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package iwona.pl.modol6email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modol6EmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
